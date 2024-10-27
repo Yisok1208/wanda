c75f809e943e2be009a6a85ce24329f31a0c0502
Yisok1208 <yng9@sheffield.ac.uk>
2024-10-27 23:32:34 +0000

    .

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_llm(model_name, cache_dir="mnt/parscratch/users/aca22yn/wanda/llm_weight
     cache_dir=cache_dir,
     low_cpu_mem_usage=True,
     device_map="auto",
-    token=hf_token,
+    use_auth_token=hf_token,
     )
 
     model.seqlen = model.config.max_position_embeddings 
