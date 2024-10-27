bf46aeefd4311eb54d2be39852fc0eb58dc03a4d
Yisok1208 <yng9@sheffield.ac.uk>
2024-10-27 23:22:57 +0000

    .

--- llama_3.2_1b.sh
+++ llama_3.2_1b.sh
@@ -17,14 +17,15 @@ module load Anaconda3/2024.02-1
 module load cuDNN/8.9.2.26-CUDA-12.1.1
 
 #Activate conda
-conda activate prune_llm
-export HUGGINGFACE_TOKEN=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
+source activate prune_llm
+
+# Set the HF_TOKEN environment variable by reading the file
+export HF_TOKEN=$(cat /mnt/parscratch/users/aca22yn/wanda/token/hf_token.txt)
 
 srun python /mnt/parscratch/users/aca22yn/wanda/main.py \
           --model meta-llama/Llama-3.2-1B-Instruct \
           --prune_method wanda \
           --sparsity_ratio 0.5 \
           --sparsity_type unstructured \
-          --save_dir "/mnt/parscratch/users/aca22yn/results/llama_3.2_1B/" \
-	  --hf_token 'cat"/mnt/parscratch/users/aca22yn/wanda/token/hf_token.txt
+          --save "/mnt/parscratch/users/aca22yn/results/llama_3.2_1B/"
 
