709e842bae3bd87a6bf8e76bb731cb65f6b792ae
Yisok1208 <yng9@sheffield.ac.uk>
2024-11-21 09:58:23 +0000

    F

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ print('# of gpus: ', torch.cuda.device_count())
 def get_llm(model_name, cache_dir="/mnt/parscratch/users/aca22yn/cache/transformers", hf_token=None):
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        torch_dtype=torch.float16,
+        torch_dtype="auto",
         cache_dir=cache_dir,
         low_cpu_mem_usage=True,
         device_map="auto",
         token=hf_token,
-        force_download=True
+        force_download=False
     )
 
     model.seqlen = model.config.max_position_embeddings 
