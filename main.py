9de24dbc3ace5f77462976334b28ddf7ff6dfa37
Yisok1208 <yng9@sheffield.ac.uk>
2024-11-24 23:53:16 +0000

    F

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ print('# of gpus: ', torch.cuda.device_count())
 
 def get_llm(model_name, cache_dir="/mnt/parscratch/users/aca22yn/cache/transformers", hf_token=None):
     model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        torch_dtype=torch.bfloat16,
+        model_name="baffo32/decapoda-research-llama-7B-hf",
+        torch_dtype=torch.float16,
         cache_dir=cache_dir,
-        device_map="cuda",
+        device_map="auto",
         token=hf_token,
-        force_download=False
+        force_download=True
     )
 
     model.seqlen = model.config.max_position_embeddings 
@@ -29,7 +29,7 @@ def get_llm(model_name, cache_dir="/mnt/parscratch/users/aca22yn/cache/transform
 def main():
     print("Script started successfully.")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default="meta-llama/Llama-3.2-1B-Instruct", help='LLaMA model')
+    parser.add_argument('--model', type=str, default="baffo32/decapoda-research-llama-7B-hf", help='LLaMA model')
     parser.add_argument('--seed', type=int, default=0, help='Seed for sampling the calibration data.')
     parser.add_argument('--nsamples', type=int, default=128, help='Number of calibration samples.')
     parser.add_argument('--sparsity_ratio', type=float, default=0, help='Sparsity level')
@@ -59,7 +59,7 @@ def main():
     model = get_llm(args.model, args.cache_dir, hf_token=os.getenv("HF_TOKEN"))
     print("Model loaded successfully.")
     model.eval()
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    tokenizer = AutoTokenizer.from_pretrained("baffo32/decapoda-research-llama-7B-hf")
     print("Tokenizer loaded successfully.")
 
     device = torch.device("cuda:0")
