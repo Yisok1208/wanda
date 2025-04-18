fb0b5c31b7399e9aa12509c0de2d0dff7c14a393
Yisok1208 <125747145+Yisok1208@users.noreply.github.com>
2025-04-18 21:43:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -98,11 +98,8 @@ def main():
     print(f"loading llm model {args.model}")
     model = get_llm(args.model, args.cache_dir)
     model.eval()
+    encoded = tokenizer(text, return_tensors='pt', truncation=True, max_length=model.config.max_position_embeddings)
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
-    if tokenizer.pad_token_id is None:            # ★ 添加
-        tokenizer.pad_token = tokenizer.eos_token
-    
     device = torch.device("cuda:0")
     if "30b" in args.model or "65b" in args.model: # for 30b and 65b we use device_map to load onto multiple A6000 GPUs, thus the processing here.
         device = model.hf_device_map["lm_head"]
