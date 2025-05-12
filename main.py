27908d52ada54017c405f9d9636dedb0c6bd518e
Yisok1208 <125747145+Yisok1208@users.noreply.github.com>
2025-05-12 01:43:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# Monkey-patch for datasets → huggingface_hub.utils.insecure_hashlib
-import hashlib, importlib
-try:
-    hfh_utils = importlib.import_module("huggingface_hub.utils")
-    setattr(hfh_utils, "insecure_hashlib", hashlib)
-except ImportError:
-    pass
-
-try:
-    hf_versions = importlib.import_module("transformers.utils.versions")
-    hf_versions.require_version = lambda *args, **kwargs: None
-except ImportError:
-    pass
-
 import argparse
 import os 
 import numpy as np
